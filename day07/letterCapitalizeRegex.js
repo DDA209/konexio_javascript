7ac22b1152c8932dbc7011e71d5bb6da8771b5a8
DDA209 <didier.jeux1@gmail.com>
2020-06-21 18:50:06 +0200

    continue exercices

--- day07/letterCapitalizeRegex.js
+++ day07/letterCapitalizeRegex.js
@@ -1,4 +1,5 @@
-function letterCapitalizeRegex(str) {
+/*
+function countWords(str) {
     var arrSentence = str.match(/\w+/gi);
     var result = arrSentence.length;
     return result;
@@ -6,10 +7,10 @@ function letterCapitalizeRegex(str) {
 var input1 = "What a wonderful world"; // 4
 var input2 = "Dit papa c'est quoi cette bouteille de lait ?"; // 9
 
-console.log(letterCapitalizeRegex(input1));
-console.log(letterCapitalizeRegex(input2));
+console.log(countWords(input1));
+console.log(countWords(input2));
 
-function letterCapitalizeRegex(str) {
+function countWordsRegex(str) {
     //
     // var temp = /[(A-Z)]\w+/gi;
     // console.log(typeof temp);
@@ -28,5 +29,20 @@ var input1 = "What a wonderful world"; // 4
 var input2 = "Dit papa c'est quoi cette bouteille de lait ?"; // 9
 "Comment ça va à Téhéran" //
 
+console.log(countWordsRegex(input1));
+console.log(countWordsRegex(input2));
+*/
+
+function letterCapitalizeRegex(str) {
+    var result = str.replace(/(\b[A-ZÇ-Ü])\w{0}/gi, str.toUpperCase(/(\b[A-ZÇ-Ü])\w{0}/gi)); //á-Ñ
+    return result;
+}
+
+var input1 = "What a wonderful world";
+var input2 = "Dit papa c'est quoi cette bouteille de lait ?";;
+var input3 = "Comment ça va à Téhéran";
+
 console.log(letterCapitalizeRegex(input1));
 console.log(letterCapitalizeRegex(input2));
+console.log(letterCapitalizeRegex(input3));
+
